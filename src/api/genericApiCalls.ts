a97f7dbba3b81fda4ae97512be782a5467510f28
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-10 13:09:02 +0200

    update

--- src/api/genericApiCalls.ts
+++ src/api/genericApiCalls.ts
@@ -4,12 +4,15 @@ import type { EndPointsKeys } from './axiosConfig';
 export async function getAxios<T>(endpoint: EndPointsKeys) {
   return await api.get<T>(endpoint);
 }
+
 export async function deleteAxios<T>(endpoint: EndPointsKeys, id: string) {
   return await api.delete<T>(`${endpoint}/${id}`);
 }
+
 export async function postAxios<T>(endpoint: EndPointsKeys, arg: T) {
   return await api.post<T>(`${endpoint}`, arg);
 }
+
 export async function putAxios<RT, BT>(endpoint: EndPointsKeys, id: string, arg: BT) {
   return await api.put<RT>(`${endpoint}/${id}`, arg);
 }
